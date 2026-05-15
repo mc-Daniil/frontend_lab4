57e36ce090c654b3e57f109c531dec50a777a6c9
danissimurr <rudakov.daniil.2004@mail.ru>
2026-05-15 19:11:05 +0300

    2.1

---
+++ tsc/example.js
@@ -0,0 +1,5 @@
+"use strict";
+function greet(person, date) {
+    alert(`Hello, ${person}! Today is ${date}.`);
+}
+greet("Dan", new Date());
---
+++ tsc/example.ts
@@ -0,0 +1,4 @@
+function greet(person: string, date: Date) {
+    alert(`Hello, ${person}! Today is ${date}.`);
+}
+greet("Dan", new Date());
